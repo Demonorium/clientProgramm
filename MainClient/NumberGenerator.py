eaea7c171f8ada6813102056a0c53490c3a76328
Demonorium <Demonorium@yandex.ru>
2021-04-19 09:37:29 +0300

    Фикс генератор

--- MainClient/NumberGenerator.py
+++ MainClient/NumberGenerator.py
@@ -14,9 +14,9 @@ class NumberGenerator:
         self.mark = {}
         self.current = (self.R + self.L) // 2
         self.step = False
-        self.start_left = self.current
+        
         self.start_right = self.current
-
+        self.start_left = self.current
 
     def get_number(self):
         self.step = not self.step
@@ -25,25 +25,26 @@ class NumberGenerator:
             self.start_right += 1
             return temp
         else:
-            temp = self.start_left
             self.start_left -= 1
-            return temp
+            return self.start_left
 
     def tip(self, data):
         if data == NumberGenerator.GREATER:
             self.L = self.start_right
             self.current = (self.R + self.L) // 2
-            self.start_left = self.current
             self.start_right = self.current
+            self.start_left = self.current
+            self.step = True
         elif data == NumberGenerator.LESS:
             self.R = self.start_left + 1
             self.current = (self.R + self.L) // 2
-            self.start_left = self.current
             self.start_right = self.current
-        else:
-            if self.step and (self.start_right < self.R):
-                self.start_right += 1
-            else:
-                self.start_left -= 1
+            self.start_left = self.current
+            self.step = True
+        #else:
+        #    if self.step and (self.start_right < self.R):
+        #        self.start_right += 1
+        #    else:
+        #        self.start_left -= 1
 
 
